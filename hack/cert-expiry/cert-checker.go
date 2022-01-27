298eebedcd2c5d9ece92bdc5638c11362fb3aec9
Ross Bryan <robryan@redhat.com>
2022-01-26 22:25:01 -0500

    refactor/clean up a bit

--- hack/cert-expiry/cert-checker.go
+++ hack/cert-expiry/cert-checker.go
@@ -8,7 +8,6 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -19,45 +18,45 @@ import (
 )
 
 var (
-	certList      []string
-	expiryList    []string
 	certDir       = "secrets"
 	certExtension = ".pem"
 )
 
 func run(ctx context.Context, l *logrus.Entry) error {
-	certList = walkCerts(ctx, l)
-	expiryList = checkCertExpiry(ctx, l, certList)
-	return nil
+	certList, err := walkCerts(ctx, l)
+	if err != nil {
+		l.Fatal(err)
+	}
+	return checkCertExpiry(ctx, l, certList)
 
 }
 
-func walkCerts(ctx context.Context, l *logrus.Entry) []string {
+func walkCerts(ctx context.Context, l *logrus.Entry) ([]string, error) {
 	var f []string
 	err := filepath.Walk(certDir, func(file string, i os.FileInfo, err error) error {
 		if strings.EqualFold(filepath.Ext(file), certExtension) {
 			l.Infof("found cert, %s", file)
 			f = append(f, file)
 		}
-		return nil
+		return err
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return f
+	return f, err
 }
 
-func checkCertExpiry(ctx context.Context, l *logrus.Entry, list []string) []string {
+func checkCertExpiry(ctx context.Context, l *logrus.Entry, list []string) error {
+	var err error
+	var r []byte
 	for i := range list {
-		r, err := ioutil.ReadFile(list[i])
+		r, err = ioutil.ReadFile(list[i])
 		if err != nil {
 			l.Fatal(err)
 		}
+		// loops over blocks in a pem file
+		// if block is a private key, skip and get next block
+		// if block != private key, its a cert we can check expiry of
 		block, rest := pem.Decode(r)
 		for block.Type == "PRIVATE KEY" {
 			block, rest = pem.Decode(rest)
-			break
 		}
 		cert, err := x509.ParseCertificate(block.Bytes)
 		if err != nil {
@@ -65,11 +64,10 @@ func checkCertExpiry(ctx context.Context, l *logrus.Entry, list []string) []stri
 		}
 		daysRemaining := cert.NotAfter.Sub(time.Now()).Hours() / 24
 		if daysRemaining < 30 {
-			expiryList = append(expiryList, cert.Subject.CommonName)
 			l.Warnf("%s expires in %v days", cert.Subject.CommonName, daysRemaining)
 		}
 	}
-	return expiryList
+	return err
 }
 
 func main() {
@@ -78,5 +76,4 @@ func main() {
 	if err := run(context.Background(), log); err != nil {
 		log.Fatal(err)
 	}
-
 }
